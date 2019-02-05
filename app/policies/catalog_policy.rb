47ae867fe34f2b0b967046f114179f9cd8df754c
Michael Quaas <michael@qua.as>
2019-02-05 23:31:55 +0100

    always show editing state of seminars (#201)

--- app/policies/catalog_policy.rb
+++ app/policies/catalog_policy.rb
@@ -2,6 +2,6 @@ class CatalogPolicy < ApplicationPolicy
   alias catalog record
 
   who_can(:index?)         { editor? || layouter? }
-  who_can(:show?)          { admin? or editor? or (layouter? and not catalog.published?) }
+  who_can(:show?)          { admin? || editor? || layouter? }
   who_can(:make_current?)  { true }
 end
