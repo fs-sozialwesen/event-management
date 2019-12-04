44434f18cea635ba4b01826eaaf50eb44b3abefe
Michael Quaas <michael@qua.as>
2019-12-04 23:51:31 +0100

    Redirect to new homepage

--- app/controllers/seminare_controller.rb
+++ app/controllers/seminare_controller.rb
@@ -1,26 +1,18 @@
 class SeminareController < ApplicationController
 
   def index
-    @catalogs = Catalog.published
-    @category = Category.published.find_by(id: params[:category_id]) #|| @catalog.categories.roots.first
-    @seminars = (@category ? @category.all_seminars : Seminar).published
-    # @seminars = @seminars.page(params[:page])
+    redirect_to 'https://www.pbw-lsa.de/fort_weiterbildung/seminare/'
   end
 
   def home
-    @catalog = Catalog
+    redirect_to 'https://www.pbw-lsa.de/fort_weiterbildung/seminare/'
   end
 
   def show
-    seminar_scope = Seminar
-    seminar_scope = seminar_scope.published unless current_user&.admin?
-    @seminar      = seminar_scope.find(params[:id]).decorate
+    redirect_to "https://www.pbw-lsa.de/seminar/detail/#{params[:id]}/"
   end
 
   def search
-    @query = params[:q]
-    @published_years = Catalog.published.pluck(:year).select { |year| year >= Date.current.year }
-    # @seminars = Seminar.published.where(year: @published_years).external_search @query
-    @seminars = Seminar.published.where(year: @published_years).ext_search @query
+    redirect_to 'https://www.pbw-lsa.de/fort_weiterbildung/seminare/'
   end
 end
